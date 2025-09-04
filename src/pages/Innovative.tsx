1ee0c25b4b53d25350e27e080ebbdce2a2249e71
hardcraft123 <hardik.thecraftpiece@gmail.com>
2025-09-04 10:45:25 +0530

    Update Innovative.tsx

--- src/pages/Innovative.tsx
+++ src/pages/Innovative.tsx
@@ -442,7 +442,7 @@ const Innovative = () => {
                               ref={popupRef}
                               className="absolute top-full right-0 mt-2 z-50 bg-white shadow-2xl overflow-hidden mobile-popup-center innovative-popup"
                               style={{
-                                width: iconNumber === 2 ? '470px' : iconNumber === 3 ? '470px' : '470px',
+                                width: iconNumber === 2 ? '470px' : iconNumber === 3 ? '470px' : '580px',
                                 maxWidth: '90vw',
                                 ...(iconNumber === 3 && { right: '-45px' })
                               }}
